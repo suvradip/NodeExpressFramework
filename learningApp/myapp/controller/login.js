2ecbc4c3bffc27e31840c80ef7072646c959c54d
suvradip saha <suvradip@fusioncharts.com>
2016-07-28 16:30:41 +0530

    -dont know why did change this lines ??

--- learningApp/myapp/controller/login.js
+++ learningApp/myapp/controller/login.js
@@ -1,13 +1,33 @@
-var loginInfo = require("../model/loginAction");
+var loginInfo = require("../model/loginAction"),
+    test;
 
 exports.loginValidate = function(user, pass, res){
     loginInfo.getAllByUser(user, function(err, rows){
     if(!err && rows.length >0){
       if(user === rows[0].username && pass === rows[0].password){
+        test = user;
         res.render('user/index', {name: user, username:user });
       }
       else
         res.render('login/index', {message: "Invalid username/password." });
     }
   });
+};
+
+
+
+exports.chartcontroler = function(socket_io, io){
+
+    
+    console.log('a user connected');
+    socket_io.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+
+    socket_io.on('chat_object', function(_chartObj){
+      var time = new Date();
+      console.log(test);
+      time = time.getHours() + ':'+ time.getMinutes();
+      io.emit('chat message', {msg:_chartObj.msg, time:time, username:_chartObj.username, posi:'right'});
+    });
 };
